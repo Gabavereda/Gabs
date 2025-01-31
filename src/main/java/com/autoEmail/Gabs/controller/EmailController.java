0c2c5486d4571cbc3d25d24e7e3b7d1b38aaf38f
Glauber Vereda <gabavereda@gmail.com>
2025-01-31 12:35:01 -0300

    Post_cadastrarEmaail

--- src/main/java/com/autoEmail/Gabs/controller/EmailController.java
+++ src/main/java/com/autoEmail/Gabs/controller/EmailController.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.autoEmail.Gabs.controller;
 
-/**
- *
- * @author glaub
- */
+import com.autoEmail.Gabs.model.Email;
+import com.autoEmail.Gabs.repository.EmailRepository;
+import com.autoEmail.Gabs.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/emails")
 public class EmailController {
-    
+
+    @Autowired
+    private EmailRepository emailRepository;
+
+    @Autowired
+    private EmailService emailService;
+
+    // cadastrar um email 
+    @PostMapping("/cadastrar")
+    public Email cadastrarEmail(@RequestBody Email email) {
+        return emailRepository.save(email);
+    }
+
 }
