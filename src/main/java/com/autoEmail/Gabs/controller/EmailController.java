9acc53f8546b39af83fa1d2fd09dfeb6dc09e88e
Glauber Vereda <gabavereda@gmail.com>
2025-01-31 12:43:01 -0300

    Criando_Post

--- src/main/java/com/autoEmail/Gabs/controller/EmailController.java
+++ src/main/java/com/autoEmail/Gabs/controller/EmailController.java
@@ -3,14 +3,17 @@ package com.autoEmail.Gabs.controller;
 import com.autoEmail.Gabs.model.Email;
 import com.autoEmail.Gabs.repository.EmailRepository;
 import com.autoEmail.Gabs.service.EmailService;
+import java.io.IOException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// Irei testar com Postman
 @RestController
 @RequestMapping("/emails")
 public class EmailController {
@@ -31,6 +34,14 @@ public class EmailController {
     @GetMapping("/listar")
     public List<Email> listarEmails() {
         return emailRepository.findAll();
+    }
+
+    @PostMapping("enviar/${id}")
+    public String enviarEmail(@PathVariable Long id) throws IOException {
+        Email email = emailRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Email não encontrado"));
+        emailService.enviarEmail(email.getDestinatario(), email.getAssunto(), email.getMessage());
+        return "Email enviado Com sucesso";
     }
 
 }
