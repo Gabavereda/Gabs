c60f669dc5ed0cab284c01ac8f4fc9f7eb378097
Glauber Vereda <gabavereda@gmail.com>
2025-01-31 12:37:54 -0300

    Get_para_Listar_Emails

--- src/main/java/com/autoEmail/Gabs/controller/EmailController.java
+++ src/main/java/com/autoEmail/Gabs/controller/EmailController.java
@@ -3,7 +3,9 @@ package com.autoEmail.Gabs.controller;
 import com.autoEmail.Gabs.model.Email;
 import com.autoEmail.Gabs.repository.EmailRepository;
 import com.autoEmail.Gabs.service.EmailService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +25,12 @@ public class EmailController {
     @PostMapping("/cadastrar")
     public Email cadastrarEmail(@RequestBody Email email) {
         return emailRepository.save(email);
+    }
+
+    // listar os emails
+    @GetMapping("/listar")
+    public List<Email> listarEmails() {
+        return emailRepository.findAll();
     }
 
 }
