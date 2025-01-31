8d4daa59f8cb6d3ee8165cf5d99186a444917210
Glauber Vereda <gabavereda@gmail.com>
2025-01-31 11:29:37 -0300

    create repo

---
+++ src/main/java/com/autoEmail/Gabs/GabsApplication.java
@@ -0,0 +1,13 @@
+package com.autoEmail.Gabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabsApplication.class, args);
+	}
+
+}
