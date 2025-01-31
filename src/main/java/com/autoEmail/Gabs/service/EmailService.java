60da1825314eddfa20f9e9698346a9e30c3f8bce
Glauber Vereda <gabavereda@gmail.com>
2025-01-31 13:48:31 -0300

    Update EmailService.java

--- src/main/java/com/autoEmail/Gabs/service/EmailService.java
+++ src/main/java/com/autoEmail/Gabs/service/EmailService.java
@@ -18,7 +18,7 @@ public class EmailService {
     public void enviarEmail(String destinatario, String assunto, String mensagem) throws IOException {
 
         // esse escopo pode mudar futuramente
-        Email from = new Email("glauber_189@hotmail.com");
+        Email from = new Email("gabavereda@gmail.com");
         Email to = new Email(destinatario);
         Content content = new Content("text/plain", mensagem);
         Mail mail = new Mail(from, assunto, to, content);
